32b5a8b8a500b43b076ef175d6681c39d8e55387
Victor Schubert <victor.schubert@doctolib.com>
2020-02-04 16:35:23 +0100

    add scratch state

--- extension/background.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener(handleMessage)
-
-function handleMessage(message, sender, sendResponse) {
-  console.log('received message', message, 'from', sender)
-  chrome.runtime.sendNativeMessage('com.doctolib.simplenotify', message)
-}
--- host/index.js
+++ host/index.js
@@ -1,11 +1,3 @@
-// load libsimplenotify
-
-const { Library } = require('ffi')
-
-const libsimplenotify = Library('./libsimplenotify.so', {
-  show_notification: ['int', ['string']],
-})
-
 // get message from browser on stdin
 
 const nativeMessaging = require('chrome-native-messaging')
@@ -13,9 +5,7 @@ const nativeMessaging = require('chrome-native-messaging')
 process.stdin
   .pipe(new nativeMessaging.Input())
   .pipe(new nativeMessaging.Transform((message, push, done) => {
-    if (message.action === 'notify')
-      libsimplenotify.show_notification(message.text)
-    done()
+    // ...
   }))
   .pipe(new nativeMessaging.Output())
   .pipe(process.stdout)
